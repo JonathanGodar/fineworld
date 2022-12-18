6331f9d67476d8f24fff4c0579117dc487696ff8
JonathanGodar <jonathan.godar@gmail.com>
2022-12-18 17:53:56 +0100

    some wierd bug with vertical collisions not being detected

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::time::Duration;
 
 use bevy::{
+    sprite::collide_aabb::{collide, Collision},
     prelude::*,
     app::{ScheduleRunnerSettings, ScheduleRunnerPlugin, AppExit}, render::camera::RenderTarget, time::FixedTimestep,
 };
@@ -16,11 +17,11 @@ fn main() {
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
-                .with_system(paddle_ai_system.before(check_for_collisions))
+                // .with_system(paddle_ai_system.before(check_for_collisions))
                 .with_system(apply_velocity.before(check_for_collisions))
                 .with_system(check_for_collisions)
             )
-        .add_system(controlled_paddle_movement_system)
+        // .add_system(controlled_paddle_movement_system)
         .add_system(escape_system)
         .run();
 }
@@ -91,26 +92,138 @@ fn setup(mut commands: Commands) {
         MainCamera,
     ));
 
-    commands.spawn((
-            create_sprite(PADDLE_COLOR, Vec3::new(LEFT_PADDLE_X_POS, 0., 0.), Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)),
-            Paddle,
-            Controlled,
-            Collider,
-    ));
+    // commands.spawn((
+    //         create_sprite(PADDLE_COLOR, Vec3::new(LEFT_PADDLE_X_POS, 0., 0.), Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)),
+    //         Paddle,
+    //         Controlled,
+    //         Collider,
+    // ));
 
-    commands.spawn((
-            create_sprite(PADDLE_COLOR, Vec3::new(RIGHT_PADDLE_X_POS, 0., 0.), Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)),
-            Paddle,
-            AI,
-            Collider
-    ));
+    // commands.spawn((
+    //         create_sprite(PADDLE_COLOR, Vec3::new(RIGHT_PADDLE_X_POS, 0., 0.), Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)),
+    //         Paddle,
+    //         AI,
+    //         Collider
+    // ));
+
+    commands.spawn_batch(create_walls(Vec2::new(-500., 500.), Vec2::new(500., -500.), 20.));
 
     commands.spawn(BallBundle {
         ball: Ball,
-        velocity: Velocity(Vec2::new(100., 100.)),
-        sprite_bundle: create_sprite(BALL_COLOR, Vec3::ZERO, Vec2::new(BALL_WIDTH, BALL_HEIGHT)),
+        velocity: Velocity(Vec2::new(150., 0.)),
+        sprite_bundle: create_sprite(BALL_COLOR, Vec3::new(-900., -500., 0.), Vec2::new(BALL_WIDTH, BALL_HEIGHT)),
         collider: Collider,
     });
+
+}
+
+fn check_collision(rect1_pos: Vec3, rect1_scale: Vec2, rect2_pos: Vec3, rect2_scale: Vec2) -> Option<Collision> {
+    let rect1_min_x = rect1_pos.x - rect1_scale.x / 2.0;
+    let rect1_max_x = rect1_pos.x + rect1_scale.x / 2.0;
+    let rect1_min_y = rect1_pos.y - rect1_scale.y / 2.0;
+    let rect1_max_y = rect1_pos.y + rect1_scale.y / 2.0;
+
+    let rect2_min_x = rect2_pos.x - rect2_scale.x / 2.0;
+    let rect2_max_x = rect2_pos.x + rect2_scale.x / 2.0;
+    let rect2_min_y = rect2_pos.y - rect2_scale.y / 2.0;
+    let rect2_max_y = rect2_pos.y + rect2_scale.y / 2.0;
+
+    if rect1_max_x < rect2_min_x || rect1_min_x > rect2_max_x || rect1_max_y < rect2_min_y || rect1_min_y > rect2_max_y {
+        return None;
+    } else {
+        println!("Not returingin none");
+    }
+
+
+    let mut collision_side = None;
+    let x_overlap = if rect1_min_x < rect2_min_x {
+        rect2_min_x - rect1_max_x
+    } else {
+        rect1_min_x - rect2_max_x
+    };
+    let y_overlap = if rect1_min_y < rect2_min_y {
+        rect2_min_y - rect1_max_y
+    } else {
+        rect1_min_y - rect2_max_y
+    };
+
+    if x_overlap < y_overlap {
+        if rect1_min_x < rect2_min_x {
+            collision_side = Some(Collision::Bottom);
+        } else {
+            collision_side = Some(Collision::Top);
+        }
+    } else {
+        if rect1_min_y < rect2_min_y {
+            collision_side = Some(Collision::Right);
+        } else {
+            collision_side = Some(Collision::Left);
+        }
+    }
+    // if x_overlap < y_overlap {
+    //     if rect1_min_x < rect2_min_x {
+    //         collision_side = Some(Collision::Right);
+    //     } else {
+    //         collision_side = Some(Collision::Left);
+    //     }
+    // } else {
+    //     if rect1_min_y < rect2_min_y {
+    //         collision_side = Some(Collision::Top);
+    //     } else {
+    //         collision_side = Some(Collision::Bottom);
+    //     }
+    // }
+
+    collision_side
+}
+
+
+#[derive(Component)]
+struct Wall {}
+
+#[derive(Bundle)]
+struct WallBundle {
+    sprite: SpriteBundle,
+    collider: Collider,
+}
+
+fn create_walls(top_left: Vec2, bottom_right: Vec2, thickness: f32) -> Vec<WallBundle> {
+    let mut walls = vec!();
+
+    let height = top_left.y - bottom_right.y;
+    let width = top_left.x - bottom_right.x;
+
+    let top = Vec2::new(top_left.x - width / 2., top_left.y);
+    let left = Vec2::new(top_left.x, top_left.y - height / 2.);
+    let bottom = Vec2::new(top_left.x - width / 2., bottom_right.y);
+    let right = Vec2::new(bottom_right.x, top_left.y - height / 2.);
+
+    let side_wall_scale = Vec2::new(thickness, height + thickness);
+
+    // // Top
+    // walls.push(WallBundle {
+    //     sprite: create_sprite(Color::GRAY, top.extend(0.), Vec2::new(width, thickness)),
+    //     collider: Collider,
+    // });
+
+    // // Left
+    // walls.push(WallBundle {
+    //     sprite: create_sprite(Color::GRAY, left.extend(0.), side_wall_scale),
+    //     collider: Collider,
+    // });
+
+    // Bottom 
+    walls.push(WallBundle {
+        sprite: create_sprite(Color::GRAY, bottom.extend(0.), Vec2::new(width, thickness)),
+        collider: Collider,
+    });
+
+    // Right 
+    walls.push(WallBundle {
+        sprite: create_sprite(Color::GRAY, right.extend(0.), side_wall_scale),
+        collider: Collider,
+    });
+    return walls;
 }
 
 
@@ -157,29 +270,32 @@ fn update_cursor_position_system(wnds: Res<Windows>, q_camera: Query<(&Camera, &
 
 fn check_for_collisions(
     mut ball_query: Query<(&Transform, &mut Velocity), With<Ball>>,
-    colliders_query: Query<(&Transform), (With<Collider>, Without<Ball>)>
+    colliders_query: Query<&Transform, (With<Collider>, Without<Ball>)>
 ){
     let (ball_transform, mut ball_vel)  = ball_query.single_mut();
 
-    {
-        let mut reflect_x = false;
-        let mut reflect_y = false;
-        if ball_transform.translation.x < -400. || ball_transform.translation.x > 400. {
-            reflect_x = true;
+    let mut reflect_x = false;
+    let mut reflect_y = false;
+    // println!("-----Checking collisions:");
+    for &transform in colliders_query.iter() {
+        // println!("{:#?}", transform.translation);
+        if let Some(collision) = check_collision(ball_transform.translation, ball_transform.scale.truncate(), transform.translation, transform.scale.truncate()) {
+            println!("found collision {:?}", collision);
+            match collision {
+                Collision::Top => reflect_y = true,
+                Collision::Bottom => reflect_y = true,
+
+                Collision::Right => reflect_x = true,
+                Collision::Left => reflect_x = true,
+
+                Collision::Inside => {},
+            }
         }
-
-        if ball_transform.translation.y < -400. || ball_transform.translation.y > 400. {
-            reflect_y = true;
-        }
-
-        for (&transform) in colliders_query.iter() {
-        }
-
-
-
-        if reflect_x { ball_vel.x *= -1. };
-        if reflect_y { ball_vel.y *= -1. };
     }
+    // println!("-----Collisions done");
+
+    if reflect_x { ball_vel.x *= -1. };
+    if reflect_y { ball_vel.y *= -1. };
 }
 
 fn paddle_ai_system(mut paddles: Query<&mut Transform, (With<AI>, With<Paddle>)>, ball: Query<&Transform, (With<Ball>, Without<Paddle>)>) {
