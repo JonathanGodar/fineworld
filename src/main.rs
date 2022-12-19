66d430c3a31012ef529f7effa16ca24e309faded
JonathanGodar <jonathan.godar@gmail.com>
2022-12-19 17:51:03 +0100

    cube at 0_0_0

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,8 @@ fn main() {
         ..default()
     }))
     .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.8)))
-    .add_startup_system(setup)
+    .add_startup_system(setup_camera)
+    .add_startup_system(setup_world)
     .add_system_set(SystemSet::new().with_run_criteria(FixedTimestep::step(TIME_STEP as f64)))
     .add_system(close_on_esc)
     .run();
@@ -31,10 +32,25 @@ fn main() {
 #[derive(Component)]
 struct MainCamera;
 
-fn setup(mut commands: Commands) {
-    setup_camera(commands);
+fn setup_camera(mut commands: Commands) {
+    commands
+        .spawn(Camera3dBundle {
+            transform: Transform::from_xyz(0., 5., -10.).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        })
+        .insert(MainCamera);
 }
 
-fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera3dBundle::default()).insert(MainCamera);
+fn setup_world(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Cube::new(5.).into()),
+        material: materials.add(StandardMaterial::from(Color::GREEN)),
+        ..Default::default()
+    });
 }
+
+fn camera_movement_system(mut q: Query<&mut Transform, With<MainCamera>>) {}
